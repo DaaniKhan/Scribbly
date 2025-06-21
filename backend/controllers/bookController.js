d03d996dbf7e7584d7c68ac702f7a72a62e9f635
Daanish Khan <70915304+DaaniKhan@users.noreply.github.com>
2025-06-21 16:56:49 +0500

    Add Book Routes and Controllers

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,86 @@
+import Book from "../models/bookModel.js"
+import mongoose from "mongoose"
+
+// get all books
+const getBooks = async (req, res) => {
+    const books = await Book.find({}).sort({createdAt: -1})
+
+    res.status(200).json(books)
+}
+
+// get a single book
+const getBook = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    const book = await Book.findById(id)
+
+    if (!book) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    res.status(200).json(book)
+}
+
+// Add new book
+const addBook = async (req, res) => {
+    const {title, author, cover, rating, notes} = req.body
+
+    try {
+        const newBook = await Book.create({title: title, author: author, cover: cover, rating: rating, notes: notes})
+        
+        res.status(200).json(newBook)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+    
+}
+
+// Delete a book
+const deleteBook = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    const book = await Book.findOneAndDelete({_id: id})
+
+    if (!book) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    res.status(200).json(book)
+}
+
+// update a book
+const updateBook = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    const book = await Book.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if (!book) {
+        return res.status(404).json({error: 'No such book'})
+    }
+
+    const newBook = await Book.findById(id)
+
+    res.status(200).json(newBook)
+}
+
+export {
+    getBooks,
+    getBook,
+    addBook,
+    deleteBook,
+    updateBook
+}
---
+++ backend/routes/bookRoutes.js
@@ -0,0 +1,27 @@
+import express from "express"
+import { 
+    getBooks,
+    getBook,
+    addBook,
+    deleteBook,
+    updateBook
+ } from "../controllers/bookController.js"
+
+const router = express.Router()
+
+// GET all books
+router.get('/', getBooks)
+
+// GET single book
+router.get("/:id", getBook)
+
+// POST a new book
+router.post("/", addBook)
+
+// DELETE a book
+router.delete("/:id", deleteBook)
+
+// UPDATE a book
+router.patch("/:id", updateBook)
+
+export default router
